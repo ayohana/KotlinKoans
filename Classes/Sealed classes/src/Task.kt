5beb082001db1a9f59365b3dc3d9fbe61540a48c
Adela Darmansyah <adela.yohana@gmail.com>
2020-07-21 17:25:24 -0700

    Fix untracked files

--- Classes/Sealed classes/src/Task.kt
+++
@@ -1,9 +0,0 @@
-fun eval(expr: Expr): Int =
-        when (expr) {
-            is Num -> TODO()
-            is Sum -> TODO()
-        }
-
-interface Expr
-class Num(val value: Int) : Expr
-class Sum(val left: Expr, val right: Expr) : Expr
